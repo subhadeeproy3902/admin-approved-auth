604f95e39c3804ae67962db3e2fbc09175f7885d
subhadeeproy3902 <subha9.5roy350@gmail.com>
2024-09-03 11:34:42 +0530

    perfect now admin left !!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/components/shared/Hero";
 import { getSession } from "@/lib/cookie";
+import { redirect } from "next/navigation";
 
 export default async function AnimatedLanding() {
   const session = await getSession();
+  if (session) {
+    redirect("/dashboard");
+    return
+  }
   const loggedIn = session ? true : false;
 
   return <Hero loggedIn={loggedIn} />;
